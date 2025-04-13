d4a752e91c6d6198ba61671459d9bc327b70a655
OrlovAlexander <oaln@yandex.ru>
2025-04-13 14:32:56 +0300

    Добавлены комментарии для конфигурирования и логирования.

--- ChatWebSocket.Server/Program.cs
+++ ChatWebSocket.Server/Program.cs
@@ -1,7 +1,9 @@
 ﻿using ChatWebSocket.Server.HandlersModels;
 using ChatWebSocket.Server.Infrastructure.Handlers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ChatWebSocket.Server;
 
@@ -9,15 +11,43 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        // // Непосредственное использование configuration builder, т.е.
+        // // не используя Hosting
+        // var config = new ConfigurationBuilder()
+        //     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //     .AddEnvironmentVariables()
+        //     .Build();
+        // var settings = 
+        //     config.GetRequiredSection("NameSection").Get<object>();
+        // settings.KeyOne;
 
+        // // Непосредственное использование logger factory, т.е.
+        // // не используя Hosting
+        // NUGET: Microsoft.Extensions.Logging
+        // NUGET: Microsoft.Extensions.Logging.Console
+        // using var factory = LoggerFactory.Create(builder => builder.AddConsole());
+        // var logger = factory.CreateLogger(nameof(Program));
+        // logger.LogInformation("Starting websocket server");
+        
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        
         builder.Services.AddScoped(typeof(IRequestMessage), typeof(RequestMessage));
         builder.Services.AddScoped(typeof(IResponseMessage), typeof(ResponseMessage));
         builder.Services.AddSingleton(typeof(IMessageHandler), typeof(AutoHandler));
         builder.Services.AddSingleton(typeof(IMessageChainHandlers), typeof(MessageChainHandlers));
         builder.Services.AddSingleton(typeof(Listener));
-        
+
         using IHost host = builder.Build();
+        
+        // // Предполагается наличие файла appsettings.json в проекте
+        // var config = host.Services.GetRequiredService<IConfiguration>();
+        // config.GetValue<type>("key:subkey");
+        // config["key:subkey"];
+
+        // // NUGET: Microsoft.Extensions.Hosting
+        // // NUGET: Microsoft.Extensions.Logging
+        // var logger = host.Services.GetRequiredService<ILogger<Program>>();
+        // logger.LogInformation("Host created.");
 
         // Запуск Слушателя
         var listener = host.Services.GetRequiredService<Listener>();
